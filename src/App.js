207e9220449d336fcabb8e9efa3685e845d334e7
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-13 20:14:01 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Slider from './components/Slider';
 function App() {
   return (
     <div className="container">
-      <div className='main-image' />
+      <div className='main-image'>Image</div>
       <div className='sidebar'>
         Sidebar
       </div>
