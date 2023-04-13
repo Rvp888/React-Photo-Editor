3e7cfa39874973a6027eaaf9b49c88d7a9289fc5
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-13 20:52:32 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,78 @@ import SidebarItem from './components/SidebarItem';
 import Slider from './components/Slider';
 
 
+const DEFAULT_OPTIONS = [
+  {
+    name: "Brightness",
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%',
+  },
+  {
+    name: "Contrast",
+    property: 'contrast',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%',
+  },
+  {
+    name: "Saturation",
+    property: 'saturation',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%',
+  },
+  {
+    name: "Grayscale",
+    property: 'grayscale',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%',
+  },
+  {
+    name: "Sepia",
+    property: 'sepia',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%',
+  },
+  {
+    name: "Hue Rotate",
+    property: 'hue-rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360
+    },
+    unit: 'deg',
+  },
+  {
+    name: "Blur",
+    property: 'blur',
+    value: 0,
+    range: {
+      min: 0,
+      max: 20
+    },
+    unit: 'px',
+  },
+]
 
 function App() {
   return (
